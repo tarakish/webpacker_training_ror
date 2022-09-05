8d2c169cefffae0836eaf6f7cb5c6aa565ecdffb
tarakish <akr.3838soccer14@gmail.com>
2022-09-05 18:41:57 +0900

    Feat: ProvidePluginの追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
+const provide = require('./plugins/provide')
 
 environment.loaders.prepend('erb', erb)
+environment.plugins.prepend('provide', provide)
 module.exports = environment
